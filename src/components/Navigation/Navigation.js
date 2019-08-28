f9f8d5441c0b48ee47993bbd6983ee8b1d42fd0d
unknown <Elliott@valtechcreative.com>
2019-08-28 19:23:10 -0400

    Navbar started

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navigation() {
+    return (
+        <header>
+            <div className="main-navigation__logo">
+                <h1>The Navbar</h1>
+            </div>
+        </header>
+    )
+}

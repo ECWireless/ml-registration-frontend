62992cb379e7f489ca25794fa4078c9ff102b901
unknown <Elliott@valtechcreative.com>
2019-08-29 00:13:21 -0400

    Nav bar added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 
 // Pages
+import Navigation from './components/Navigation/Navigation';
+
 import AuthPage from './pages/Auth';
 import RegisterPage from './pages/Register';
 import FormPage from './pages/Form';
@@ -11,12 +13,15 @@ import FormPage from './pages/Form';
 function App() {
 	return (
 		<BrowserRouter>
-			<Switch>
-				<Redirect from="/" to='/login' exact />
-				<Route path="/login" component={AuthPage} />
-				<Route path="/register" component={RegisterPage} />
-				<Route path="/form" component={FormPage} />
-			</Switch>
+			<Navigation />
+			<main className="main-content">
+				<Switch>
+					<Redirect from="/" to='/login' exact />
+					<Route path="/login" component={AuthPage} />
+					<Route path="/register" component={RegisterPage} />
+					<Route path="/form" component={FormPage} />
+				</Switch>
+			</main>
 		</BrowserRouter>
 	);
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './Navigation.css';
 
 export default function Navigation() {
     return (
-        <header>
+        <header className="main-navigation">
             <div className="main-navigation__logo">
-                <h1>The Navbar</h1>
+                <h1>Merrill Lynch Registration</h1>
             </div>
+            <nav className="main-navigation__items">
+                <ul>
+                    <li><NavLink to="/register">Registration</NavLink></li>
+                    <li><NavLink to="/form">Form</NavLink></li>
+                    <li><NavLink to="/login">Login</NavLink></li>
+                </ul>
+            </nav>
         </header>
     )
 }
